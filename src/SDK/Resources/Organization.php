c1aecb3d22f52feeb07774b48565265764c52259
Jamie Howard <jamiehowardco@gmail.com>
2018-03-09 14:52:48 -0500

    Correct array offset issues.

--- src/SDK/Resources/Organization.php
+++ src/SDK/Resources/Organization.php
@@ -12,7 +12,7 @@ class Organization extends AbstractResource
     public function find(int $id)
     {
         try {
-            return $this->client->get($this->entityName . "/$id")['data']['organizations'][0];
+            return $this->client->get($this->entityName . "/$id")['data']['organization'];
         } catch (\RequestException $e) {
             return false;
         }
