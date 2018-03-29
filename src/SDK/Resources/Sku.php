78e942a4cc6ee5a3882ec0ba8c021498f28d6a2b
Jamie Howard <jamiehowardco@gmail.com>
2018-03-29 15:15:28 -0400

    Added searching skus by key support

---
+++ src/SDK/Resources/Sku.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace RethinkGroup\SDK\Resources;
+
+class AccessControl extends AbstractResource
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function find(int $id)
+    {
+        //
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function store(array $data)
+    {
+        //
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function update(int $id, array $data, bool $force = false)
+    {
+        //
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function delete(int $id)
+    {
+        //
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function search(string $term, string $searchFields = null)
+    {
+        $params = [];
+        $params['search'] = trim($term);
+
+        if ( ! is_null($searchFields) ) {
+            $params['searchFields'] = $searchFields;
+        }
+
+        return  $this->client->get('skus', $params)['data']['skus'];
+    }
+
+    public function getByKey($key)
+    {
+        return $this->search($key, 'key:like');
+    }
+}
