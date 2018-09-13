81b4dbdb08b9bcb983cb61007f0dec8a7e7df950
Jamie Howard <jamiehowardco@gmail.com>
2018-09-13 06:37:30 -0400

    [OBRT-2] Added access control searching

--- src/SDK/Resources/AccessControl.php
+++ src/SDK/Resources/AccessControl.php
@@ -7,4 +7,28 @@ class AccessControl extends Resource
     protected $entityName = 'accessControls';
 
     protected $singularEntityName = 'accessControl';
+
+    /**
+     * Search for a SOUR record.
+     *
+     * @param  array  $fields organization_id, user_id, sku_id
+     * @return array
+     */
+    public function accessControlSearch($fields = [])
+    {
+    	$terms = '';
+    	$searchFields = '';
+
+    	$i = 0;
+    	foreach ($fields as $key => $value) {
+    		$terms .= ($i > 0) ? 'and|' : '';
+    		$terms .= "$key:$value;";
+
+    		$searchFields .= "$key:=;";
+
+    		$i++;
+    	}
+
+    	return $this->search($terms, $searchFields);
+    }
 }
